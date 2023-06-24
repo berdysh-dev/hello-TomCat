86ccc9cedcf6adb1aaced358bb3e257acff58397
root <root@berdysh.net>
2023-06-24 12:26:43 +0900

    Test

--- setenv.sh
+++ setenv.sh
@@ -3,8 +3,8 @@
 export LANG=C
 export TZ=JST-9
 
-export JAVA_HOME=/usr/lib/jvm/java
-export JRE_HOME=/usr/lib/jvm/java
+export JAVA_HOME=/usr/lib/jvm
+export JRE_HOME=/usr/lib/jvm
 
 export TOMCAT_HOME=/usr/local/tomcat
 export CATALINA_HOME=/usr/local/tomcat
